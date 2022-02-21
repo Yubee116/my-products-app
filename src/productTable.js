276174c9597fc28b5141900d4cda2976f254d20c
Yubee116 <ubongnehemiah@gmail.com>
2022-02-21 04:58:30 +0100

    fix compile warning

--- src/productTable.js
+++ src/productTable.js
@@ -36,7 +36,7 @@ class ProductTable extends React.Component{
         let isAsc = this.state.sort.direction === 'asc' ? -1 : 1
         let [a,b] = [objectA[this.state.sort.column], objectB[this.state.sort.column]]
         if (this.state.sort.column === 'price'){
-            [a,b] =  [a,b].map((value)=> parseFloat(value.replace(/[^\d\.]/g, ''), 10))
+            [a,b] =  [a,b].map((value)=> parseFloat(value.replace(/[^\d.]/g, ''), 10))
         }
         if (a < b){
             return 1 * isAsc
